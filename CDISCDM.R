9473c459d964027b3e0511838f90edf5346555e7
LIHAO <85012206+lee1995hao@users.noreply.github.com>
2025-11-26 13:44:12 +0900

    add the attr
    final_verision

--- CDISCDM.R
+++ CDISCDM.R
@@ -11,10 +11,10 @@ dm_data
 
 
 dm_data_1 <- dm_data %>% mutate(study_id_new = "STUDY001",
-                   subject_id_new = as.character(str_c("SUB", str_pad(1:100,side = "left",width = 4,pad = "0"))))
-  
-                   
-                   
+                                subject_id_new = as.character(str_c("SUB", str_pad(1:100,side = "left",width = 4,pad = "0"))))
+
+
+
 dm_data_2 <- dm_data_1 %>% left_join(max_min_dataset, by = c("subject_id_new" = "subject_id"))%>%
   rename(rfstdtc = max_date,rfscdtc = min_date)
 
@@ -23,7 +23,7 @@ dead_time <- ae_data %>%
   summarise(max_date = max(as.Date(end_date)))
 
 
-dm_data_2 %>% left_join(dead_time, by = c("subject_id_new" = "subject_id")) %>% 
+final_data <- dm_data_2 %>% left_join(dead_time, by = c("subject_id_new" = "subject_id")) %>% 
   mutate(dthfl = if_else(is.na(max_date),"", "Y")) %>%
   mutate(dthdtc =  as.character(max_date)) %>% select(-max_date) %>%
   mutate(site_id = str_sub(study_id,-3,-1)) %>%
@@ -39,11 +39,43 @@ dm_data_2 %>% left_join(dead_time, by = c("subject_id_new" = "subject_id")) %>%
   mutate(ethnicity_new = "Not Hispanic or Latino") %>%
   mutate(actam = if_else(tttt == 0,"placedo", "treatment")) %>%
   mutate(armcd = if_else(tttt == 0,str_to_upper("pla"),str_to_upper("tre"))) %>% 
-  select(-tttt)
+  select(-tttt) %>% 
+  select(c(study_id_new,subject_id_new,rfstdtc,rfscdtc,dthfl,dthdtc,site_id,age,ageu,sex,ethnicity_new,actam,armcd))
+
+excel_data <- data.frame(
+  Variable_Name = c(
+    "STUDYID", "DOMAIN", "USUBJID",
+    "BRTHDTC", "SEX", "AGE",
+    "AETERM", "AEDECOD", "AESTDTC", "AEENDTC","subjectid"
+  ),
+  Variable_Label = c(
+    "Study Identifier",
+    "Domain Abbreviation",
+    "Unique Subject Identifier",
+    "Date/Time of Birth",
+    "Sex",
+    "Age",
+    "Reported Term for the Adverse Event",
+    "Dictionary-Derived Term",
+    "Start Date/Time of Adverse Event",
+    "End Date/Time of Adverse Event",
+    "butongrendeid"
+  )
+)
+
+
+
+connect_name_label <- function(x){
+  colname <- cur_column()
   
+  if(colname %in% excel_data$Variable_Name){
+    idxxx <- which(excel_data$Variable_Name == colname)
+    attr(x,"label") <- excel_data$Variable_Label[idxxx]
+  }
+  return(x)
+}
 
-  
+output_final_data <- final_data %>% mutate(across(everything(), connect_name_label))
 
-
-
+write_xpt(output_final_data,"out_DM.xpt")
 
